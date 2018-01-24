3fc4f62fa60fc1fd12b30b9564e5cddd98f517c8
MinesJA <minesja@gmail.com>
2018-01-23 22:06:27 -0500

    split up coin functions, added return currnt prices function, split up user functions

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -2,24 +2,38 @@ class Coin < ActiveRecord::Base
   has_many :coin_transactions
   has_many :users, through: :coin_transactions
 
-  def self.update_or_create
+  def self.start_import
     if Coin.all.empty?
       get_json.each do |coin_instance|
-        Coin.create(coin_name: coin_instance["name"], coin_price: coin_instance["price_usd"], coin_marketcap: coin_instance["market_cap_usd"])
-      end
-      else
-        Coin.all.each do |coin|
-          api_coin = get_json.find do |api_coin|
-            coin.coin_name == api_coin["name"]
-          end
-          coin.coin_price = api_coin["price_usd"]
-          coin.coin_marketcap = api_coin["market_cap_usd"]
-          coin.save
-          puts "#{coin.coin_name} | #{coin.coin_price} | #{coin.coin_marketcap} "
-        end
-      end; nil
+        name = coin_instance["name"]
+        price = coin_instance["price_usd"]
+        marketcap = coin_instance["market_cap_usd"]
 
+        Coin.create(coin_name: name, coin_price: price, coin_marketcap: marketcap)
+      end
     end
+  end
+  
+
+  def self.update_or_create
+    if Coin.all.any?
+
+      Coin.all.each do |coin|
+        api_coin = get_json.find {|api_coin| coin.coin_name == api_coin["name"]}
+
+        coin.coin_price = api_coin["price_usd"]
+        coin.coin_marketcap = api_coin["market_cap_usd"]
+        coin.save
+      end; nil
+    end
+  end
+
+
+  def self.return_current_prices
+    Coin.all.each do |coin|
+      puts "#{coin.coin_name}........#{coin.coin_price}"
+    end
+  end
 
     # def self.all_prices
     #   Coin.all.map do |coin|
@@ -27,4 +41,4 @@ class Coin < ActiveRecord::Base
     #   end
     # end
 
-  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,6 @@ class User < ActiveRecord::Base
 
 
 
-
-
-
-
 ############-----USD TRANSACTIONS---------##################
   #USD TRANSACTIONS SCHEMA
   # create_table "usd_transactions", force: :cascade do |t|
@@ -21,7 +17,7 @@ class User < ActiveRecord::Base
 
 
   def create_usdtransaction(usd_amount, type)
-    #Type = "Deposit" || "Exchange" (Buys or Sells)
+    #Type = "Deposit" || "Buy" || "Sell"
     #Buys = -usd_amount
     #Sells = +usd_amount
     #Deposits = +usd_amount
@@ -39,14 +35,30 @@ class User < ActiveRecord::Base
     end
   end
 
+
   def users_deposits
     #returns array of all USD Transactions that are deposits
-
     self.users_usdtransactions.select do |usdtransaction|
       usdtransaction.usd_transaction_type == "Deposit"
     end
-
   end
+
+
+  def users_buys
+    self.users_usdtransactions.select do |usdtransaction|
+      usdtransaction.usd_transaction_type == "Buy"
+    end
+  end
+
+
+  def users_sells
+    self.users_usdtransactions.select do |usdtransaction|
+      usdtransaction.usd_transaction_type == "Sell"
+    end
+  end
+
+  #To cut down on code we could combine users_buys, users_sells, users_deposits
+  #into one and pass in the string ("Buy" "Sell" "Deposit") in as an argument
 
 
   def return_cash_on_hand
@@ -58,6 +70,10 @@ class User < ActiveRecord::Base
   def return_total_deposited_cash
     self.users_deposits.inject(0){|sum,x| sum + x }
   end
+
+  def return_growth_rate
+
+    
 
 
 
