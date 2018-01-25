dea9158242f28684995cbcb56465b172ee1d4d47
MinesJA <minesja@gmail.com>
2018-01-25 11:32:03 -0500

    Added deposit function to CLI

--- app/models/CLI.rb
+++ app/models/CLI.rb
@@ -80,6 +80,13 @@ class CLI
     end
   end
 
+  def create_or_sign_in
+    puts "Would you like to sign in or log in to an existing account?"
+
+    puts "1. Create a new account. 2. Log in to an existing account. 3. Exit."
+    response = gets.chomp
+  end
+
 
   def no_matching_username
     puts "I'm sorry, we couldn't find that username."
@@ -106,33 +113,54 @@ class CLI
     puts "Enter the number associated with your choice below:"
     puts "1. View Account. 2. Deposit USD. 3. See leaderboard. 4. Buy coins. 5. Sell Coins. 6. Watch prices. 7. Exit "
 
-    case response
-    when "1"
-      create_account
-    when "2"
-      signin
-    when "3"
-      exit
-    else
-      puts "I'm sorry, I didn't get that."
-      create_or_sign_in
-    end
-
-  end
-
-  def create_or_sign_in
-    puts "Would you like to sign in or log in to an existing account?"
-
-    puts "1. Create a new account. 2. Log in to an existing account. 3. Exit."
     response = gets.chomp
 
+    case response
+    when "1"
+      #returns account page
+      self.current_user.show_user_balance
+    when "2"
 
+
+      deposit
+    when "3"
+      leaderboard
+    when "4"
+      buy_coins
+    when "5"
+      sell_coins
+    when "6"
+      watch_prices
+    else
+      puts "I'm sorry, I didn't get that."
+      account_menu
+    end
   end
 
 
+  def deposit
+    puts "Sure thing. How much USD would you like to deposit?"
+    usd_amount = gets.chomp
+    if usd_amount.downcase == "exit"
+      exit
+    else
+      usd_amount = gets.chomp.to_f.round(2)
+
+      if usd_amount > 0
+        self.current_user.deposit_usd(usd_amount)
+      else
+        puts "I'm sorry, I didn't get that."
+        puts "You need to enter a valid number greater than 0."
+        deposit
+      end
+  end
 
 
+    else
+      puts "I'm sorry, I didn't get that. You have to input a number"
 
+
+  end
 
 
   def exit
